b62fd1426debd256cc150e4a421ecded75c8c790
John Wason <wason@wasontech.com>
2023-08-25 03:45:51 -0400

    Add build scripts

---
+++ recipe/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p build2
+cd build2
+
+cmake -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+  -DCMAKE_PREFIX_PATH:PATH=$PREFIX -DCMAKE_BUILD_TYPE:STRING=Release \
+  -DCMAKE_INSTALL_LIBDIR=lib ..
+
+cmake --build . --config Release -- -j$CPU_COUNT
+cmake --build . --config Release --target install
